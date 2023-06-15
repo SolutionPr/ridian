117eb88934620ba2f313a2f6215abe79e10558c4
Solution Provider <solutions.provider.dev@gmail.com>
2023-06-15 17:29:04 +0530

    Pushed Changes to Dev Branch #1

--- app.py
+++ app.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return '<h1 style="text-align:center;">Welcome to Ridian!</h1>'
+
+
+@app.route('/dev')
+def dev():
+    return '<h1 style="text-align:center;">Welcome to Ridian (Dev)!</h1>'
